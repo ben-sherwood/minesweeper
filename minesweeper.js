e383027ac96151a482bf2bd1ae9b00c4713f0a20
ben-sherwood <ben.sherwood0@gmail.com>
2019-05-07 22:57:18 +1200

    board created

--- minesweeper.js
+++ minesweeper.js
@@ -1,10 +1,24 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
-// var board = 
+var board = {
+  cells: [
+    { row: 0, col: 0, isMine: false, hidden: false, },
+    { row: 0, col: 1, isMine: false, hidden: false, },
+    { row: 0, col: 2, isMine: false, hidden: false, },
+    { row: 1, col: 0, isMine: false, hidden: false, },
+    { row: 1, col: 1, isMine: false, hidden: false, },
+    { row: 1, col: 2, isMine: false, hidden: false, },
+    { row: 2, col: 0, isMine: false, hidden: false, },
+    { row: 2, col: 1, isMine: false, hidden: false, },
+    { row: 2, col: 2, isMine: false, hidden: false, },],
+};
 
-function startGame () {
+function startGame() {
   // Don't remove this function call: it makes the game work!
+  for (var i = 0; i<board.cells.length; i++){
+    board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
+  }
   lib.initBoard()
 }
 
@@ -12,7 +26,7 @@ function startGame () {
 //
 // 1. Are all of the cells that are NOT mines visible?
 // 2. Are all of the mines marked?
-function checkForWin () {
+function checkForWin() {
 
   // You can use this function call to declare a winner (once you've
   // detected that they've won, that is!)
@@ -27,6 +41,14 @@ function checkForWin () {
 //
 // It will return cell objects in an array. You should loop through 
 // them, counting the number of times `cell.isMine` is true.
-function countSurroundingMines (cell) {
+function countSurroundingMines(cell) {
+  var surrounding = lib.getSurroundingCells(cell.row, cell.col);
+  var count = 0;
+  for (var i = 0; i<surrounding.length; i++){
+    if (surrounding[i].isMine){
+      count++;
+    }
+  }
+  return count;
 }
 
